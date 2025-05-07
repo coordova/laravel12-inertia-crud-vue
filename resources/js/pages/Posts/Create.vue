624b43477d6374157487e764169c00e03cdcc7dd
coordova <grcordova@gmail.com>
2025-05-07 17:49:02 -0400

    refactor: clean up import statements and formatting in Post Create and Index components

--- resources/js/pages/Posts/Create.vue
+++ resources/js/pages/Posts/Create.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
-import { Head, Link, useForm } from '@inertiajs/vue3';
 import InputError from '@/components/InputError.vue';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link, useForm } from '@inertiajs/vue3';
 import { LoaderCircle } from 'lucide-vue-next';
 
 // Breadcrumbs
@@ -33,44 +33,36 @@ const submit = () => {
         onFinish: () => {
             form.reset();
         },
-
     });
 };
-
 </script>
 
 <template>
     <Head title="Post Create" />
-    
+
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-        <!-- Create a Post Button -->
-        <div>
-            <Link
-                :href="route('posts.index')"
-                class="rounded-sm bg-gray-700 px-3 py-2 text-xs font-medium text-white hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-                Go Back
-            </Link>
-        </div>
+            <!-- Create a Post Button -->
+            <div>
+                <Link
+                    :href="route('posts.index')"
+                    class="rounded-sm bg-gray-700 px-3 py-2 text-xs font-medium text-white hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                >
+                    Go Back
+                </Link>
+            </div>
             <div class="overflow-x-auto">
                 <form @submit.prevent="submit">
                     <div class="grid gap-2">
                         <Label for="email">Title</Label>
-                        <Input
-                            id="title"
-                            type="text"
-                            class="mt-1 block w-full"
-                            v-model="form.title"
-                            placeholder="Title"
-                        />
+                        <Input id="title" type="text" class="mt-1 block w-full" v-model="form.title" placeholder="Title" />
                         <InputError class="mt-2" :message="form.errors.title" />
                     </div>
-                    <div class="grid gap-2 my-4">
+                    <div class="my-4 grid gap-2">
                         <Label for="email">Body</Label>
                         <textarea
                             id="body"
-                            class="mt-1 block w-full file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-15 min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                            class="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input mt-1 block h-15 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
                             v-model="form.body"
                             placeholder="Body"
                         ></textarea>
--- resources/js/pages/Posts/Index.vue
+++ resources/js/pages/Posts/Index.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link } from '@inertiajs/vue3';
 import { Post } from '@/types/oox.d';
-import { usePage } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 // import { LoaderCircle } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -21,7 +20,7 @@ const breadcrumbs: BreadcrumbItem[] = [
     }
 );  */
 // define props - forma 1.1
-defineProps({posts: Array});
+defineProps({ posts: Array<Post>() });
 
 // define props - forma 2
 // const props = defineProps<{ posts: Post[] }>();
@@ -29,23 +28,22 @@ defineProps({posts: Array});
 // get posts - forma 3
 // const page = usePage<{ posts: Post[] }>();
 // const posts = page.props.posts;
-
 </script>
 
 <template>
     <Head title="Posts" />
-    
+
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-        <!-- Create a Post Button -->
-        <div>
-            <Link
-                :href="route('posts.create')"
-                class="rounded-sm bg-green-700 px-3 py-2 text-xs font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 focus:outline-none dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-            >
-                Create Post
-            </Link>
-        </div>
+            <!-- Create a Post Button -->
+            <div>
+                <Link
+                    :href="route('posts.create')"
+                    class="rounded-sm bg-green-700 px-3 py-2 text-xs font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 focus:outline-none dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                >
+                    Create Post
+                </Link>
+            </div>
             <div class="overflow-x-auto">
                 <table class="w-full text-left text-sm text-zinc-500 rtl:text-right dark:text-zinc-400">
                     <thead class="bg-zinc-50 text-xs text-zinc-700 uppercase dark:bg-zinc-700 dark:text-zinc-400">
@@ -66,7 +64,11 @@ defineProps({posts: Array});
                             <td class="px-6 py-2 text-zinc-600 dark:text-zinc-300">{{ post.title }}</td>
                             <td class="px-6 py-2 text-zinc-600 dark:text-zinc-300">{{ post.body }}</td>
                             <td class="px-6 py-2">
-                                <Link :href="route('posts.edit', post.id)" class=" rounded-lg bg-blue-700 px-3 py-2 text-xs font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</Link>
+                                <Link
+                                    :href="route('posts.edit', post.id)"
+                                    class="rounded-lg bg-blue-700 px-3 py-2 text-xs font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    >Edit</Link
+                                >
                                 <button
                                     class="ml-1 cursor-pointer rounded-lg bg-red-700 px-3 py-2 text-xs font-medium text-white hover:bg-red-800 focus:ring-4 focus:ring-red-300 focus:outline-none dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                                 >
